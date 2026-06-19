0ea5c7627f85a7cdf3c0ddbc1428ddc442b932c4
yamazakk1 <1218bv@gmail.com>
2026-06-19 03:36:20 +0300

    fixing 4

--- start.sh
+++ start.sh
@@ -7,13 +7,16 @@ for f in migrations/*.up.sql; do
     psql $DB_DSN -f "$f"
 done
 
+
 /app/seed
+
+export REDIS_ADDR=$(echo "$REDIS_ADDR" | sed 's|redis://||' | sed 's|.*@||')
+echo "Redis addr: $REDIS_ADDR"
 
 export AUTH_ADDR=localhost:8081
 export ASSET_ADDR=localhost:8082
 export MARKET_ADDR=localhost:8083
 export TRADING_ADDR=localhost:8084
-
 
 PORT=8081 /app/auth &
 PORT=8082 /app/asset &
