26330c444ce80689e37523d1f26ae5c6411f5012
yamazakk1 <1218bv@gmail.com>
2026-06-19 03:24:44 +0300

    render update

--- start.sh
+++ start.sh
@@ -9,19 +9,17 @@ done
 
 /app/seed
 
-export REDIS_HOST=$(echo $REDIS_ADDR | sed 's|redis://.*@||' | sed 's|:6379||')
-export REDIS_ADDR_PARSED="${REDIS_HOST}:6379"
-
 export AUTH_ADDR=localhost:8081
 export ASSET_ADDR=localhost:8082
 export MARKET_ADDR=localhost:8083
 export TRADING_ADDR=localhost:8084
 
+
 PORT=8081 /app/auth &
 PORT=8082 /app/asset &
 PORT=8083 /app/market &
 PORT=8084 /app/trading &
-REDIS_ADDR=$REDIS_ADDR_PARSED PORT=8085 /app/ws-hub &
+PORT=8085 /app/ws-hub &
 PORT=8080 /app/gateway &
 
 wait
